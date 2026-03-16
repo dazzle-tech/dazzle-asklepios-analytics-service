c48240f26f1ed37c1f782b1e264fc65ff7f79972
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-03-16 12:15:50 +0200

    collected Sample Label

---
+++ src/main/java/com/dazzle/asklepios/repository/DiagnosticOrderTestRepository.java
@@ -0,0 +1,22 @@
+package com.dazzle.asklepios.repository;
+import com.dazzle.asklepios.domain.DiagnosticOrderTest;
+import com.dazzle.asklepios.domain.enumeration.DiagnosticOrderTestStatus;
+import com.dazzle.asklepios.domain.enumeration.DiagnosticStatus;
+import com.dazzle.asklepios.domain.enumeration.TestType;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+
+@Repository
+public interface DiagnosticOrderTestRepository extends JpaRepository<DiagnosticOrderTest, Long>,
+        JpaSpecificationExecutor<DiagnosticOrderTest> {
+
+}
