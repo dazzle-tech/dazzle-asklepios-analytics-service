0819b26b2f05a94dd8f4ee91cac696df30cdd21d
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-03-16 09:58:30 +0200

    delete no usage files

--- src/main/java/com/dazzle/asklepios/service/dto/medicalsheets/diagnosticorders/requests/commands/DiagnosticTestRequestLinkDiagnosticTestDTO.java
+++
@@ -1,7 +0,0 @@
-package com.dazzle.asklepios.service.dto.medicalsheets.diagnosticorders.requests.commands;
-
-import org.wildfly.common.annotation.NotNull;
-
-public record DiagnosticTestRequestLinkDiagnosticTestDTO(
-        @NotNull Long diagnosticTestId
-) {}
--- src/main/java/com/dazzle/asklepios/service/dto/medicalsheets/diagnosticorders/requests/commands/DiagnosticTestRequestRejectDTO.java
+++
@@ -1,7 +0,0 @@
-package com.dazzle.asklepios.service.dto.medicalsheets.diagnosticorders.requests.commands;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DiagnosticTestRequestRejectDTO(
-        @NotBlank String rejectedReason
-) {}
