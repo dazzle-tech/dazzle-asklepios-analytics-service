ab6bb864723a4531bcba6263ddbbb63f8463c1b5
Rawan-Yahya200 <rawanyahya300@gmail.com>
2026-09-01 11:22:47 +0300

    fixes

--- src/main/java/com/dazzle/asklepios/repository/DiagnosticOrderRepository.java
+++ src/main/java/com/dazzle/asklepios/repository/DiagnosticOrderRepository.java
@@ -15,5 +15,4 @@ public interface DiagnosticOrderRepository extends JpaRepository<DiagnosticOrder
 
     List<DiagnosticOrder> findByPatientId(Long patientId);
     List<DiagnosticOrder> findByPatientIdOrderByCreatedDateDesc(Long patientId);
-    List<DiagnosticOrder> findByPatientId(Long patientId);
 }
